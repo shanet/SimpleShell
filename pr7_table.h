98e0128f8daa0e37a4bd9440c7591811d3227909
Gage Ames <gra5028@psu.edu>
2012-03-31 15:32:16 -0400

    Added process table implementation.  Needs work.

---
+++ pr7_table.h
@@ -0,0 +1,57 @@
+#ifndef PR7_TABLE_H
+#define PR7_TABLE_H
+
+/* CMPSC 311 Project 7, Process Table
+ *
+ * Authors:    Shane Tully and Gage Ames
+ * Emails:     spt5063@psu.edu and gra5028@psu.edu
+ *
+ * Interface information for a process table
+ */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define STATE_NONE       0
+#define STATE_RUNNING    1
+#define STATE_TERMINATED 2
+
+static char *state[] = { "none", "running", "terminated" };
+
+typedef struct child_process {
+   pid_t pid;            /* process ID, supplied from fork() */
+   int   state;          /* process state, your own definition */
+   int   exit_status;    /* supplied from wait() if process has finished */
+     /* Later... add more information */
+
+   struct child_process *next;
+     /* Later... replace array with linked list */
+} child_process_t;
+
+typedef struct process_table {
+   int children;
+   child_process_t *ptab;
+} process_table_t;
+
+/*----------------------------------------------------------------------------*/
+
+/* process table for background processes */
+
+int number_of_children(process_table_t *pt);
+
+/* return NULL if not successful */
+process_table_t *allocate_process_table(void);
+
+/* ignore possible errors */
+void deallocate_process_table(process_table_t *pt);
+
+/* return 0 if successful, -1 if not */
+int print_process_table(process_table_t *pt);
+int insert_new_process(process_table_t *pt, pid_t pid);
+int update_existing_process(process_table_t *pt, pid_t pid, int exit_status);
+int remove_old_process(process_table_t *pt, pid_t pid);
+
+/*----------------------------------------------------------------------------*/ 
+
+#endif
