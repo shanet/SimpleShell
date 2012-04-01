af42c8660d7cdbdf7803a460109835543e92e77d
Gage Ames <gra5028@psu.edu>
2012-04-01 00:18:35 -0400

    Added pjobs command and a program name to the process table.

--- pr7.3.c
+++ pr7.3.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[]) {
 
    verbose = 0;
    echo = 0;
-   int interactive = 1;
+   int interactive = 0;
    int custom_startup = 0;
    char *startup_file = STARTUP_FILE;
 
@@ -192,11 +192,15 @@ int eval_line(char *cmdline) {
    // Parent waits for foreground job to terminate
    if(background) {
       printf("background process %d: %s", (int) pid, cmdline);
-      insert_new_process(ptable, pid);
+      insert_new_process(ptable, pid, argv[0]);
    } else {
       if(waitpid(pid, &status, 0) == -1) {
          printf("%s: failed: %s\n", argv[0], strerror(errno));
          exit(EXIT_FAILURE);
+      }
+
+      if (verbose) {
+          print_wait_status(pid, status);
       }
    }
 
@@ -245,12 +249,10 @@ int parse(char *buf, char *argv[]) {
 // If first arg is a builtin command, run it and return true
 int builtin(char *argv[]) {
    // Exit command
-   if(strcmp(argv[0], "exit") == 0 || strcmp(argv[0], "quit") == 0) {
-      if(verbose) {
-         printf("%s %d: goodbye, world\n", prog, self_pid);
-      }
+   if (strcmp(argv[0], "exit") == 0 || strcmp(argv[0], "quit") == 0) {
       Exit(0);
-   } else if(strcmp(argv[0], "echo") == 0) {
+      return 0;
+   } else if (strcmp(argv[0], "echo") == 0) {
       // Print (echo) each argument
       unsigned int i=1;
       while(argv[i] != NULL) {
@@ -260,10 +262,14 @@ int builtin(char *argv[]) {
       // Print a trailing a newline
       printf("\n");
       return 0;
+   } else if (strcmp(argv[0], "pjobs") == 0) {
+      // Print the process table of running jobs
+      print_process_table(ptable);
+      return 0;
    }
 
    // ignore singleton &
-   if(strcmp(argv[0], "&") == 0) {
+   if (strcmp(argv[0], "&") == 0) {
       return 0;
    }
 
@@ -277,9 +283,12 @@ int builtin(char *argv[]) {
 // exiting
 void Exit(int status) {
    if (ptable->children != 0) {
-      printf("There %s %d background job%s running\n",
+      printf("There %s %d background job%s running.\n",
              ((ptable->children > 1) ? "are" : "is"), ptable->children,
              ((ptable->children > 1) ? "s" : "" ));
+      if (verbose) {
+         print_process_table(ptable);
+      }
    } else {
       deallocate_process_table(ptable);
       exit(status);
@@ -290,7 +299,7 @@ void Exit(int status) {
 
 void print_wait_status(pid_t pid, int status) {
    printf("process %d, completed %snormally, status %d\n", pid,
-          ((status == 0) ? "" : "ab"), status);
+          ((status >= 0) ? "" : "ab"), status);
 }
 
 //Find all the child processes that have terminated, without waiting.
